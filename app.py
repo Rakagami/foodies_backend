8a5beb697d9205efa9ce1da1cf47b0c143ec0369
Prakita Rayyan Renatin <ge25zow@mytum.de>
2021-10-14 19:51:39 +0200

    port should work plsplspls

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if os.getenv("PORT") is None:
-    port = 5000
-else:
-    port = os.getenv("PORT")
-
 @app.route('/')
 @app.route('/hello')
 def hello():
@@ -22,5 +17,6 @@ def port():
 if __name__ == "__main__":
     app.run(
             debug=True,
-            host="0.0.0.0"
+            host="0.0.0.0",
+            port=5000
             )
