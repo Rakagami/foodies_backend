2325b2c082587965aec6afce7039eb8d6e2eedd9
Prakita Rayyan Renatin <ge25zow@mytum.de>
2021-10-14 19:17:11 +0200

    Changed port

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+if os.getenv("PORT") is None:
+    port = 5000
+else:
+    port = os.getenv("PORT")
+
 # get name value from query string and cookie
 @app.route('/')
 @app.route('/hello')
@@ -15,5 +20,5 @@ if __name__ == "__main__":
     app.run(
             debug=True,
             host="0.0.0.0",
-            port=5000
+            port=port
             )
