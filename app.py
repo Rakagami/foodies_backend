1025f236ecc7ea2a9ced6b0ce564097a0aadc5c0
Prakita Renatin <ge25zow@mytum.de>
2021-10-15 12:43:35 +0200

    Added mocked response

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +14,18 @@ port = int(os.environ.get("PORT", 5000))
 def hello():
     return "Hello World!"
 
+@app.route('/all', methods = ['POST']) 
+def all():
+    response_data = request.get_json()
+    if response_data is None:
+        return {}, 400
+    food_list = response_data["foodList"]
+    for a in food_list:
+        print("food item " + str(a))
+
+    # Creating mocked response
+    mocked_response_json = json.load(open('mocked_response.json'))
+    return jsonify(mocked_response_json), 200
 
 if __name__ == "__main__":
     app.run(
