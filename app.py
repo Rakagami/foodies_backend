0b96699206c1467d68369a7d8d761c24947ba891
Maria Pospelova <mariyapospelova@gmail.com>
2021-10-16 23:48:11 +0200

    Adding recipe recommender

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
+from recipe_recommender import recommendRecipes
 
 import json
 
@@ -12,14 +13,17 @@ cors = CORS(app)
 port = int(os.environ.get("PORT", 5000))
 
 # Debug related
+
+
 @app.route('/')
 @app.route('/hello')
 def hello():
     return "Hello World!"
 
-@app.route('/all', methods = ['POST', 'OPTIONS']) 
+
+@app.route('/all', methods=['POST', 'OPTIONS'])
 def all():
-    if request.method == "OPTIONS": # CORS preflight
+    if request.method == "OPTIONS":  # CORS preflight
         return _build_cors_preflight_response()
     response_data = request.get_json()
     if response_data is None:
@@ -29,8 +33,18 @@ def all():
         print("food item " + str(a))
 
     # Creating mocked response
-    mocked_response_json = json.load(open('mocked_response.json'))
-    return _corsify_actual_response(jsonify(mocked_response_json)), 200
+    response = recommendRecipes(food_list)
+    responseDict = {"result": response.to_dict(orient="records")}
+    # responseDict = json.loads(response.to_json(orient="values"))
+
+
+    print(responseDict)
+
+    # mocked_response_json = json.load(open('mocked_response.json'))
+    # print(response.head())
+    # print(response.to_json())
+    return _corsify_actual_response(jsonify(responseDict)), 200
+
 
 def _build_cors_preflight_response():
     response = make_response()
@@ -44,9 +58,10 @@ def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
+
 if __name__ == "__main__":
     app.run(
-            debug=True,
-            host="0.0.0.0",
-            port=port
-            )
+        debug=True,
+        host="0.0.0.0",
+        port=port
+    )
